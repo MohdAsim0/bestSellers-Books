1921f620465f86ff9141710409ee0a38b8178845
MohdAsim0 <asim75077@gmail.com>
2023-09-09 02:28:29 +0530

    new

---
+++ bestSellersBooks/src/books.js
@@ -0,0 +1,31 @@
+import img1 from  './images/img1.jpg'
+import img2 from './images/img2.jpg'
+import img3 from './images/img3.jpg'
+
+
+const books = [
+    {
+      title: "Grandma's Stories",
+      author: "Sudha Murty",
+      img: img1,
+      id: 1,
+    },
+  
+    {
+      title: "The Housemaid",
+      author: "Freida McFadden",
+      img:img2,
+      id: 2,
+    },
+
+    {
+        title: "The Laws of Power",
+        author: "Robert Greene",
+        img:img3,
+        id: 3,
+
+    },
+
+  ];
+
+  export default books;
